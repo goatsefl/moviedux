58308288ba92168b52af1443d4f7dc5e7d9b0a66
goatsefl <swaroopd502@gmail.com>
2025-01-02 23:37:10 +0530

    Added most of the functionality for the working project

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,11 +1,20 @@
 import React from "react";
 import '../styles.css'
+import MovieCard from "./MovieCard";
 
 
-export default function Watchlist() {
+export default function Watchlist({ movies, watchList, toggleWatchList }) {
     return (
         <div>
             <h1 className="title">Your Watchlist</h1>
+            <div className="watchlist">
+                {
+                    watchList.map(id => {
+                        const movie = movies.find(movie => movie.id === id)
+                        return <MovieCard key={id} movie={movie} toggleWatchList={toggleWatchList} isWatchListed={true}></MovieCard>
+                    })
+                }
+            </div>
         </div>
     );
 }
